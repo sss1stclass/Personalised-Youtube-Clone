9ce85776409cf8ccf91358389411ba2a460faaee
sss1stclass <sohilkr88@gmail.com>
2025-01-11 12:11:03 +0530

    added the basic rendering and loading of videos

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import { Typography } from '@mui/material';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from '@mui/material';
+
+import { ChannelDetail, VideoDetail, SearchFeed, Navbar, Feed } from './components';
 
 const App = () => (
-  <Typography>sab sahi chl rha hai</Typography>
+  <BrowserRouter>
+    <Box sx={{ backgroundColor: '#000' }}>
+      <Navbar />
+      <Routes>
+        <Route exact path='/' element={<Feed />} />
+        <Route path='/video/:id' element={<VideoDetail />} />
+        <Route path='/channel/:id' element={<ChannelDetail />} />
+        <Route path='/search/:searchTerm' element={<SearchFeed />} />
+      </Routes>
+    </Box>
+  </BrowserRouter>
 );
 
 export default App;
